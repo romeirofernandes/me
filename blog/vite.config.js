6f53fa3bdc3d86e5c26ac9d53ee6531dc7c6e355
Romeiro <theromeirofernandes@gmail.com>
2026-05-15 14:29:33 +0530

    feat: remove unused react-scan plugin from Vite configuration and clean up dependencies

--- blog/vite.config.js
+++ blog/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-import reactScan from '@react-scan/vite-plugin-react-scan'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss(), reactScan({ autoDisplayNames: true })],
+   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
