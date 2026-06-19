5689cf4a2fb9e40e0b183e7170a21bb7b47f4622
Romeiro <theromeirofernandes@gmail.com>
2026-06-20 01:18:12 +0530

    ui updates

--- blog/src/components/Border1.jsx
+++ blog/src/components/Border1.jsx
@@ -1,13 +1,13 @@
 const Border1 = ({ children }) => (
   <div className="relative w-full">
     {/* Top left corner */}
-    <span className="absolute -top-px -left-px block size-6 border-dashed border-t border-l border-muted-foreground z-30 pointer-events-none" />
+    <span className="absolute -top-px -left-px block size-6 border-dashed border-t border-l border-zinc-600 light:border-zinc-400 z-30 pointer-events-none" />
     {/* Top right corner */}
-    <span className="absolute -top-px -right-px block size-6 border-dashed border-t border-r border-muted-foreground z-30 pointer-events-none" />
+    <span className="absolute -top-px -right-px block size-6 border-dashed border-t border-r border-zinc-600 light:border-zinc-400 z-30 pointer-events-none" />
     {/* Bottom left corner */}
-    <span className="absolute -bottom-px -left-px block size-6 border-dashed border-b border-l border-muted-foreground z-30 pointer-events-none" />
+    <span className="absolute -bottom-px -left-px block size-6 border-dashed border-b border-l border-zinc-600 light:border-zinc-400 z-30 pointer-events-none" />
     {/* Bottom right corner */}
-    <span className="absolute -bottom-px -right-px block size-6 border-dashed border-b border-r border-muted-foreground z-30 pointer-events-none" />
+    <span className="absolute -bottom-px -right-px block size-6 border-dashed border-b border-r border-zinc-600 light:border-zinc-400 z-30 pointer-events-none" />
     {children}
   </div>
 );
